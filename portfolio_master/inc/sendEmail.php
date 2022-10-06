81d3d60d6fdbf5d3586a56072cc066c93c81f45f
Aditya Galiara <91758643+Aditya07198@users.noreply.github.com>
2022-10-06 01:19:47 -0400

    Update sendEmail.php

--- portfolio_master/inc/sendEmail.php
+++ portfolio_master/inc/sendEmail.php
@@ -49,7 +49,7 @@ if($_POST) {
       ini_set("sendmail_from", $siteOwnersEmail); // for windows server
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
-	if ($mail) { echo "Email sent successfully"; }
+	if ($mail) { echo "OK"; }
       	else { echo "Something went wrong. Please try again."; }
 		
 	} # end if - no validation error
